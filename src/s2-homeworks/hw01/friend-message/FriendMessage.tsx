9768eab6aad1d46581e04e376ff0286121780a4b
“dimisini” <dimisini@gmail.com>
2023-01-09 09:10:40 -0700

    Initial commit

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
+
+export type FriendMessageType = {
+    message: MessageType
+}
+
+// создать тип вместо any и отобразить приходящие данные
+const FriendMessage = (props: FriendMessageType) => {
+    return (
+        <div
+            id={'hw1-friend-message-' + props.message.id}
+            className={s.friendMessage}
+        >
+            <div className={s.friendImageAndText}>
+                <img
+                    id={'hw1-friend-avatar-' + props.message.user.avatar}
+                    // создаёт студент
+
+                    //
+                />
+                <div className={s.friendText}>
+                    <div
+                        id={'hw1-friend-name-' + props.message.user.name}
+                        className={s.friendName}
+                    >
+                        {/*создаёт студент*/}
+
+                        {/**/}
+                    </div>
+                    <pre
+                        id={'hw1-friend-text-' + props.message.message.text}
+                        className={s.friendMessageText}
+                    >
+                        {/*создаёт студент*/}
+
+                        {/**/}
+                    </pre>
+                </div>
+            </div>
+            <div
+                id={'hw1-friend-time-' + props.message.message.time}
+                className={s.friendTime}
+            >
+                {/*создаёт студент*/}
+
+                {/**/}
+            </div>
+        </div>
+    )
+}
+
+export default FriendMessage
